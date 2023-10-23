3901fdbe32e7082d210416c71a92cee0216016a9
David Renassia <drenassi@student.42perpignan.fr>
2023-10-23 16:31:13 +0200

    GNL with bonus

---
+++ get_next_line_bonus.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: drenassi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/21 18:14:04 by drenassi          #+#    #+#             */
+/*   Updated: 2023/10/21 18:19:55 by drenassi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+#include <stdio.h>
+
+char	*ft_stash(char *stash)
+{
+	int		i;
+	char	*res;
+
+	i = 0;
+	if (!stash)
+		return (NULL);
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	if (stash[i] == '\n')
+		i++;
+	res = gnl_substr(stash, i, gnl_strlen(stash) - i + 1);
+	free(stash);
+	if (res[0] == 0)
+	{
+		free(res);
+		res = NULL;
+	}
+	return (res);
+}
+
+char	*ft_get_line(char *stash)
+{
+	int		i;
+	char	*line;
+
+	i = 0;
+	if (!stash)
+		return (NULL);
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	if (stash[i] == '\n')
+		i++;
+	line = gnl_substr(stash, 0, i);
+	return (line);
+}
+
+char	*ft_read(int fd, char *buffer, char *stash)
+{
+	int		r;
+
+	r = 1;
+	while (r && !gnl_strchr(stash, '\n'))
+	{
+		r = read(fd, buffer, BUFFER_SIZE);
+		if (r <= -1)
+		{
+			free(buffer);
+			free(stash);
+			return (NULL);
+		}
+		if (r == 0)
+			break ;
+		buffer[r] = '\0';
+		stash = gnl_strjoin(stash, buffer);
+		if (!stash)
+		{
+			free(buffer);
+			return (NULL);
+		}
+	}
+	free(buffer);
+	return (stash);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*stash[1024];
+	char		*buffer;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+		return (free(stash[fd]), stash[fd] = NULL, NULL);
+	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer)
+		return (NULL);
+	stash[fd] = ft_read(fd, buffer, stash[fd]);
+	line = ft_get_line(stash[fd]);
+	stash[fd] = ft_stash(stash[fd]);
+	return (line);
+}
